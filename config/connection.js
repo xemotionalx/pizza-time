b418032d7de989ddd85f6ae6a52b2a95e6b8c690
maiada <maiadacx@gmail.com>
2019-11-13 21:44:29 -0500

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "yourRootPassword",
-    database: "pizza_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "", //Your User
+        password: "", //Your Password
+        database: "burgers_db" //Your Database
+    });
+};
 
 connection.connect(function(err) {
     if (err) {
